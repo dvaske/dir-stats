06217e9276a8ea676755054718cf7abc37625aef
Aske Olsson <askeolsson@gmail.com>
2011-09-06 11:23:37 +0200

    Directory statistics, initial commit
    Signed-off-by: Aske Olsson <askeolsson@gmail.com>

---
+++ dir-stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+dir-stats.py
+
+Run analysis on directory
+
+Created by Aske Olsson 2011-09-05.
+Copyright (c) 2011 Aske Olsson. All rights reserved.
+
+
+"""
+
+
+def analyze_dir(content):
+    pass
+
+def get_dir_content():
+    content = None
+
+    return content
+
+
+
